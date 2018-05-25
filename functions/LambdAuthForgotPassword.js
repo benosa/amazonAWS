9fa59b0b38a8a30baceef8c152a014ad56614f1a
Igor Dronov <id@bevolved.net>
2018-05-25 16:21:57 +0300

    new commit<F2>

--- functions/LambdAuthForgotPassword.js
+++ functions/LambdAuthForgotPassword.js
@@ -8,58 +8,7 @@ const resposeObject = require('../libs/response');
 global.fetch = require('node-fetch')
 
 class CognitoHelper {
-    constructor() {
-        this.poolData = { "UserPoolId": config.UserPoolId, "ClientId":config.ClientId };
-        this.userPool = new ACI.CognitoUserPool(this.poolData);
-    }
-    forgotPassword(username, req){
-        try{
-			let userData = {
-				Username : username,
-				Pool : this.userPool
-            };
-            let cognitoUser = new ACI.CognitoUser(userData);
-            return new Promise(function(resolve) {
-                let ro = new resposeObject();
-                cognitoUser.forgotPassword({
-                    onSuccess: function (data) {
-                        // successfully initiated reset password request
-                        console.log('CodeDeliveryData from forgotPassword: ' + data);
-                        ro.data = {"CodeDeliveryData from forgotPassword" : CircularJSON.stringify(data)};
-                        ro.status = 200;
-                        ro.message = "Password successfully change";	
-                        Logger.broadcast("info", "CognitoHelper -> forgotPassword - successfully! : " + CircularJSON.stringify(result));
-                        return resolve(ro);
-                    },
-                    onFailure: function(err) {
-                        //alert(err.message || JSON.stringify(err));
-                        console.log(err);
-                        Logger.broadcast("error", "CognitoHelper -> forgotPassword - fail! : " + CircularJSON.stringify(err));
-                        ro.message = CircularJSON.stringify(err);
-						return resolve(ro);
-                    },
-                    //Optional automatic callback
-                    inputVerificationCode: function(data) {
-                        console.log('Code sent to: ' + data);
-                        var verificationCode = prompt('Please input verification code ' ,'');
-                        var newPassword = prompt('Enter new password ' ,'');
-                        cognitoUser.confirmPassword(verificationCode, newPassword, {
-                            onSuccess() {
-                                console.log('Password confirmed!');
-                            },
-                            onFailure(err) {
-                                console.log('Password not confirmed!');
-                            }
-                        });
-                    }
-                });
-			}.bind(this));
-        }catch(ex){
-            Logger.broadcast("error", "CognitoHelper -> changePassword - general error! : " + CircularJSON.stringify(ex));
-            let ro = new resposeObject();
-			ro.message = CircularJSON.stringify(ex);
-			return ro;
-        }
+    
     }
     
 }
